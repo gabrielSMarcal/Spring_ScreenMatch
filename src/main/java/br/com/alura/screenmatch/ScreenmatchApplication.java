24f12ccd1269c9317fa0256a35b669b6ee2dbc10
yhamerith <gabriel.santos.marcar1@gmail.com>
2025-02-07 09:32:35 -0300

    Consumo de API

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,6 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.service.ApiConsumer;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +14,12 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hello World!");
+
+		var api = new ApiConsumer();
+		var json = api.obterDados("https://www.omdbapi.com/?t=gilmore+girls&Season=1&apikey=6585022c");
+		System.out.println(json);
+
+		json = api.obterDados("https://coffee.alexflipnote.dev/random.json");
+		System.out.println(json);
 	}
 }
