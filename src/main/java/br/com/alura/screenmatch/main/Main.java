ae3ed6baf6d7e3f03edf83907756cdc0434aa5ae
yhamerith <gabriel.santos.marcar1@gmail.com>
2025-02-08 10:41:08 -0300

    Exemplo de Steam

--- src/main/java/br/com/alura/screenmatch/main/Main.java
+++ src/main/java/br/com/alura/screenmatch/main/Main.java
@@ -4,6 +4,7 @@ import br.com.alura.screenmatch.model.*;
 import br.com.alura.screenmatch.service.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -18,36 +19,46 @@ public class Main {
 
     public void menu() {
 
-        System.out.println("Bem-vindo ao Screen Match!");
-        System.out.println("*****************************************************************************************");
-        System.out.println("Digite o nome da série que deseja buscar:");
-
-        var serieName = scanner.nextLine();
-        var json = api.getData(ADDRESS + serieName.replace(" ", "+") + API_KEY);
-        var data = conversor.getData(json, SerieData.class);
-        System.out.println(data);
-        System.out.println("*****************************************************************************************");
-
-        List<SeasonData> seasons = new ArrayList<>();
-        for (int i = 1; i <= data.totalTemporadas(); i++) {
-
-            json = api.getData(ADDRESS + serieName.replace(" ", "+") +
-                    "&Season=" + i + API_KEY);
-            SeasonData season = conversor.getData(json, SeasonData.class);
-            seasons.add(season);
-        }
-        seasons.forEach(System.out::println);
-        System.out.println("*****************************************************************************************");
-
-//        for (int i = 0; i < data.totalTemporadas(); i++) {
+//        System.out.println("Bem-vindo ao Screen Match!");
+//        System.out.println("*****************************************************************************************");
+//        System.out.println("Digite o nome da série que deseja buscar:");
 //
-//            List<EpisodeData> seasonEpisodes = seasons.get(i).episodios();
-//            for (int j = 0; j < seasonEpisodes.size(); j++) {
-//                System.out.println(seasonEpisodes.get(j).titulo());
-//            }
+//        var serieName = scanner.nextLine();
+//        var json = api.getData(ADDRESS + serieName.replace(" ", "+") + API_KEY);
+//        var data = conversor.getData(json, SerieData.class);
+//        System.out.println(data);
+//        System.out.println("*****************************************************************************************");
+//
+//        List<SeasonData> seasons = new ArrayList<>();
+//        for (int i = 1; i <= data.totalTemporadas(); i++) {
+//
+//            json = api.getData(ADDRESS + serieName.replace(" ", "+") +
+//                    "&Season=" + i + API_KEY);
+//            SeasonData season = conversor.getData(json, SeasonData.class);
+//            seasons.add(season);
 //        }
+//        seasons.forEach(System.out::println);
+//        System.out.println("*****************************************************************************************");
+//
+////        for (int i = 0; i < data.totalTemporadas(); i++) {
+////
+////            List<EpisodeData> seasonEpisodes = seasons.get(i).episodios();
+////            for (int j = 0; j < seasonEpisodes.size(); j++) {
+////                System.out.println(seasonEpisodes.get(j).titulo());
+////            }
+////        }
+//
+//        // Lambda expression
+//        seasons.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
 
-        // Lambda expression
-        seasons.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+
+        // Exemplo de Stream
+        List<String> nomes = Arrays.asList("Ana", "Maria", "João", "Pedro", "Henrique");
+        nomes.stream()
+                .sorted()
+                .limit(3)
+                .filter(n -> n.startsWith("J"))
+                .map(n -> n.toUpperCase())
+                .forEach(System.out::println);
     }
 }
