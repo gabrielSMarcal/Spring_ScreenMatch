07fdb6dea4c44af9f49b023f8c74fb30ee4e8308
yhamerith <gabriel.santos.marcar1@gmail.com>
2025-02-09 08:26:40 -0300

    Busca por episódios

--- src/main/java/br/com/alura/screenmatch/main/Main.java
+++ src/main/java/br/com/alura/screenmatch/main/Main.java
@@ -55,39 +55,52 @@ public class Main {
                 .flatMap(s -> s.episodios().stream())
                 .collect(Collectors.toList());
 
-        System.out.println("\n10 melhores episódios:");
-        episodesData.stream()
-                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
-                .peek(e -> System.out.println("Primeiro filtro(N/A)" + e))
-                .sorted(Comparator.comparing(EpisodeData::avaliacao).reversed())
-                .peek(e -> System.out.println("Ordenação: " + e))
-                .limit(10)
-                .peek(e -> System.out.println("Limitação: " + e))
-                .map(e -> e.titulo().toUpperCase())
-                .peek(e -> System.out.println("Mapeamento: " + e))
-                .forEach(System.out::println);
+//        System.out.println("\n10 melhores episódios:");
+//        episodesData.stream()
+//                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+//                .peek(e -> System.out.println("Primeiro filtro(N/A)" + e))
+//                .sorted(Comparator.comparing(EpisodeData::avaliacao).reversed())
+//                .peek(e -> System.out.println("Ordenação: " + e))
+//                .limit(10)
+//                .peek(e -> System.out.println("Limitação: " + e))
+//                .map(e -> e.titulo().toUpperCase())
+//                .peek(e -> System.out.println("Mapeamento: " + e))
+//                .forEach(System.out::println);
 
         List<Episode> episodes = seasons.stream()
                 .flatMap(s -> s.episodios().stream()
                         .map(d -> new Episode(s.temporada(), d))
                 ).collect(Collectors.toList());
 
-        // episodes.forEach(System.out::println);
+        episodes.forEach(System.out::println);
 
-        System.out.println("A partir de que ano deseja buscar episódios?");
-        var year = scanner.nextInt();
-        scanner.nextLine();
+        System.out.println("Procure episódio pelo título:");
+        var titlePart = scanner.nextLine();
+        Optional<Episode> searchEpisode = episodes.stream()
+                .filter(e -> e.getTitle().toUpperCase().contains(titlePart.toUpperCase()))
+                .findFirst();
 
-        LocalDate dateSearch = LocalDate.of(year, 1, 1);
+        if (searchEpisode.isPresent()) {
+            System.out.println("Episódio encontrado: " + searchEpisode.get());
+        } else {
+            System.out.println("Episódio não encontrado.");
+        }
 
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        episodes.stream()
-                .filter(e -> e.getLaunch() != null && e.getLaunch().isAfter(dateSearch))
-                .forEach(e -> System.out.println(
-                        "Temporada: " + e.getSeason() +
-                                " - Episódio: " + e.getEpisodeNumber() +
-                                " - Data de lançamento: " + e.getLaunch().format(dtf)
-                ));
+
+//        System.out.println("A partir de que ano deseja buscar episódios?");
+//        var year = scanner.nextInt();
+//        scanner.nextLine();
+//
+//        LocalDate dateSearch = LocalDate.of(year, 1, 1);
+//
+//        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+//        episodes.stream()
+//                .filter(e -> e.getLaunch() != null && e.getLaunch().isAfter(dateSearch))
+//                .forEach(e -> System.out.println(
+//                        "Temporada: " + e.getSeason() +
+//                                " - Episódio: " + e.getEpisodeNumber() +
+//                                " - Data de lançamento: " + e.getLaunch().format(dtf)
+//                ));
 
     }
 }
